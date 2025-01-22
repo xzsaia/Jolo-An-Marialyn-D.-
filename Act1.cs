08766595db2caf1f2287cc547b1166a1b0568678
Marialyn Jolo-An <157707699+xzsaia@users.noreply.github.com>
2025-01-22 16:58:28 +0800

    Add files via upload

---
+++ Act1.cs
@@ -0,0 +1,26 @@
+Using System;
+
+namespace Activity1
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+		
+		var days = new[] {"Day 1 Monday", "Day 2 Tuesday", "Day 3 Wednesday", 
+						"Day 4 Thursday", "Day 5 Friday", "Day 6 Saturday",
+		"Day 7 Sunday" };
+		
+		
+		Console.WriteLine(days[0]);
+		Console.WriteLine(days[1]);	
+		Console.WriteLine(days[2]);
+		Console.WriteLine(days[3]);		
+		Console.WriteLine(days[4]);
+		Console.WriteLine(days[5]);
+		Console.WriteLine(days[6]);
+		
+		
+		}
+	}
+}
---
+++ Jolo-An, Marialyn DIT.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] textPosts = { "Cat, Dog, Rabbit, Cow, Sheep, Mouse, Chicken, Frog, Bird, Kangaroo" };
+
+            for (int i = 0; i < textPosts.Length; i++)
+            {
+                Console.WriteLine(textPosts[i]);
+                Console.WriteLine("Choose A or B: ");
+                Console.WriteLine("a) React");
+                Console.WriteLine("b) Skip");
+                char input = Convert.ToChar(Console.ReadLine());
+
+                if (input == 'a')
+                {
+                    Console.WriteLine("a) Like");
+                    Console.WriteLine("b) Dislike");
+                    char output = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine("ADDED");
+                }
+                else if (input == 'b')
+                {
+                    Console.WriteLine("ADDED");
+                }
+                for (int i = 0; i < n - i; 1++)
+                {
+                    for (int j = 0; j < n - 1 - 1 ) ;
+                }
+            }
+        }
+    }
+}
