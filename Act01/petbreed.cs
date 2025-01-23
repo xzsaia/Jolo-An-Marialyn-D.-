311ffb18343f6353883d3bfab8ff8f994484cb86
Marialyn Jolo-An <157707699+xzsaia@users.noreply.github.com>
2025-01-23 20:52:34 +0800

    Add files via upload

---
+++ Act01/petbreed.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Act01
+{
+    internal class petbreed
+    {
+        public string pbreed { get; set; }
+
+
+        public override string ToString()
+        {
+            return pbreed;
+        }
+    }
+}
---
+++ Act01/petnames.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act01
+{
+    internal class petnames
+    {
+        public string pOwner { get; set; }
+        public string pnames { get; set; }
+        public string pGender{ get; set; }
+
+        public override string ToString()
+        {
+            return pOwner + " " + pnames +  " " + pGender;
+        }
+
+    }
+}
